1a0d6b4493be01a9b2b2f32f34c065247b187760
Carlos Gonzalez <gonzalez2.cag@gmail.com>
2021-04-05 12:52:48 -0400

    hacer que se muestres los puntos asi no tenga puntos

--- leopardus/resources/views/wallet/indexwalletpuntos.blade.php
+++ leopardus/resources/views/wallet/indexwalletpuntos.blade.php
@@ -17,11 +17,11 @@
                     <div class="col-12 col-md-6">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">{{($wallet->status == 1)? 'Puntos Pagados' : 'Puntos Pendientes'}}</h4>
+                                <h4 class="card-title">{{($wallet['status'] == 1)? 'Puntos Pagados' : 'Puntos Pendientes'}}</h4>
                             </div>
                             <div class="card-body">
-                                <h5 class="card-text">Puntos Derechos: <strong>{{ $wallet->der }}</strong></h5>
-                                <h5 class="card-text">Puntos Izquierdos: <strong>{{$wallet->izq }}</strong></h5>
+                                <h5 class="card-text">Puntos Derechos: <strong>{{ $wallet['der'] }}</strong></h5>
+                                <h5 class="card-text">Puntos Izquierdos: <strong>{{$wallet['izq'] }}</strong></h5>
                             </div>
                         </div>
                     </div>
